0bd2415f3ad74661a35f7d38db5557aeb07ecd9a
Peter Yoon <peyo.hd@gmail.com>
2017-08-03 09:29:52 -0400

    Enable GLSurfaceView
    Currently mesa3d vc4 build is supporting only following formats.
    
    - RGB 565, Alpha 0, Depth 24 or 0, Stencil 8 or 0
    - RGB 888, Alpha 8, Depth 24 or 0, Stencil 8 or 0

--- opengl/java/android/opengl/GLSurfaceView.java
+++ opengl/java/android/opengl/GLSurfaceView.java
@@ -992,7 +992,7 @@ public class GLSurfaceView extends SurfaceView implements SurfaceHolder.Callback
      */
     private class SimpleEGLConfigChooser extends ComponentSizeChooser {
         public SimpleEGLConfigChooser(boolean withDepthBuffer) {
-            super(8, 8, 8, 0, withDepthBuffer ? 16 : 0, 0);
+            super(8, 8, 8, 8, withDepthBuffer ? 24 : 0, 0);
         }
     }
 
