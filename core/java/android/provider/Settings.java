90741cdecf454bcf98dcaa6f2330893b9a6b24c6
Brett Rogers <brettrogers11@gmail.com>
2017-10-16 16:32:24 +0200

    Status bar notification ticker [1/2]
    * abc ezio84: cleanup and port to Oreo
    * du nychitman1: cleanup and port to Nougat
    
    Change-Id: Idfc5c3a9398d0590ea0ef32c8a6efbca8513a1e8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4037,6 +4037,12 @@ public final class Settings {
         public static final String ACCENT_PICKER = "accent_picker";
 
         /**
+         * Whether to show the notification ticker on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_TICKER = "status_bar_show_ticker";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
