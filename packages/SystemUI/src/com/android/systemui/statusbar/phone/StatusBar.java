f3416e4346852d784c1d52157e32e87d344b5472
Kshitij Gupta <kshitijgm@gmail.com>
2018-01-22 22:50:18 -0500

    Add Settings Dark UI
    Change-Id: Ida9af23606765f9567dcc70502e6ee9a43f899ca

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2070,6 +2070,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     public boolean isUsingDarkTheme() {
         OverlayInfo themeInfo = null;
         try {
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.settings.theme.dark",
+                    mLockscreenUserManager.getCurrentUserId());
             themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark",
                     mLockscreenUserManager.getCurrentUserId());
         } catch (RemoteException e) {
@@ -3862,6 +3864,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (isUsingDarkTheme() != useDarkTheme) {
             mUiOffloadThread.submit(() -> {
                 try {
+                    mOverlayManager.setEnabled("com.android.settings.theme.dark",
+                            useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                     mOverlayManager.setEnabled("com.android.system.theme.dark",
                             useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                 } catch (RemoteException e) {
