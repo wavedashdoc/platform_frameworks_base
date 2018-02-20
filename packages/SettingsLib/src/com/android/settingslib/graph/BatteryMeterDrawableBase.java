0f83994956916766fa5b4fd3ec253233a830d051
ezio84 <brabus84@gmail.com>
2018-02-21 00:42:55 +0100

    Portrait battery: improve size for the warning level exclamation mark
    Change-Id: I7b1e6456a721a961ad01a7bab1086b4bac069c1d

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -253,6 +253,8 @@ public class BatteryMeterDrawableBase extends Drawable {
 
         mHeight = (bounds.bottom - mPadding.bottom) - (bounds.top + mPadding.top);
         mWidth = (bounds.right - mPadding.right) - (bounds.left + mPadding.left);
+        mWarningTextPaint.setTextSize(mHeight * 0.75f);
+        mWarningTextHeight = -mWarningTextPaint.getFontMetrics().ascent;
 
         switch (mMeterStyle) {
             case BATTERY_STYLE_PORTRAIT:
